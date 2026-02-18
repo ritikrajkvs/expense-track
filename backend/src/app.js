72b3d07fb2a829188c03070ea6d1d56147e38913
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2026-02-18 22:23:30 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://expensedtrack123.netlify.app",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/expenses', expensesRoutes);
