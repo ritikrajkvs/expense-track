184671e1172a8db9d58b06a4d29c70b7c85052ce
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2026-02-18 23:27:31 +0530

    Update expenses.routes.js

--- backend/src/routes/expenses.routes.js
+++ backend/src/routes/expenses.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createExpense, getExpenses } from '../controllers/expenses.controller.js';
+import { createExpense, getExpenses, deleteExpense } from '../controllers/expenses.controller.js';
 import { validateExpense } from '../middlewares/validate.middleware.js';
 
 const router = express.Router();
 
 router.post('/', validateExpense, createExpense);
 router.get('/', getExpenses);
+router.delete('/:id', deleteExpense);
 
 export default router;
